e72e267b0e48a8337fdc48435698e54c1df2e873
Soolecki <40840914+soolecki@users.noreply.github.com>
2019-09-27 13:21:58 +0200

    Ikony aplikacji

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,178 @@
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/service-worker.js')
+           .then(function() { console.log("Service Worker Registered"); });
+}
+
+// Dom7
+var $$ = Dom7;
+
+// Framework7 App main instance
+var app  = new Framework7({
+  root: '#app',
+  id: 'io.framework7.artweb.okazje',
+  name: 'Okazje',
+  theme: 'md',
+  routes: routes,
+  precompileTemplates: false,
+  template7Pages: true,  
+  lazy:{
+    threshold: 800,
+    sequential: false
+  },
+  pushState: true,
+  panel:{
+    swipe: 'left',
+  }
+});
+
+//var filtersView = app.views.create('.panel-left .view');
+
+$$(document).on('DOMContentLoaded', function() { 
+  init();
+});
+
+/*$$(document).on('deviceready', function() {
+    StatusBar.hide();
+    init();
+    setTimeout(showNotification,25000);
+});*/
+
+
+function showNotification(){
+  cordova.plugins.notification.local.schedule({
+    title: 'Sprawdź najnowsze okazje!',
+    text: 'Treść powiadomienia.',
+    foreground: true
+  });
+}
+
+function init(){
+
+    $$('#app').on('change', '#filters_form select', function (e) {
+
+      if(e.target.value=='default') $$(e.target).closest('li').removeClass('no-default'); else $$(e.target).closest('li').addClass('no-default');
+
+    });
+
+    $$('#app').on('click','#filters_form_advanced_toggle', function(e){
+      $$('.page-searchform').toggleClass('advanced');
+      if($$('.page-searchform').hasClass('advanced')){
+        $$('[name="mode"]').val('advanced').change();
+      }else{
+        $$('[name="mode"]').val('simple').change(); 
+      }
+    });
+
+    $$('#app').on('click', '#filters_form_submit', function (e) {
+
+      /*if(e.target.value=='default') $$(e.target).closest('a').removeClass('no-default'); else $$(e.target).closest('a').addClass('no-default');
+      if($$(e.target).hasClass('no-route'))return false;*/
+
+      inputs=$$('#filters_form select,#filters_form input');
+      var str = "";
+      inputs.each(function (i, item) { str += encodeURIComponent(item.name) + "=" + encodeURIComponent(item.value) + "&"; });
+      app.router.navigate("/okazje/lists/"+str);
+
+    });    
+
+    /*$$('.filters-reset').click(function(){
+      inputs=$$('#filters_form select');
+      inputs.each(function(i,item) {
+          $$(item).val('default').addClass('no-route').change().removeClass('no-route');
+      });
+      app.router.navigate('/home/');
+    });*/
+
+    $$('#app').on('click', '.share', function (e) {
+      if (navigator.share) { 
+        navigator.share({
+            title: 'Web Fundamentals',
+            text: 'Check out Web Fundamentals — it rocks!',
+            url: 'https://developers.google.com/web',
+        })
+          .then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      }   
+    });
+
+    $$('#app').on('click', '.favorite', function (e) {
+      url = mainView.history[mainView.history.length-1];
+      favobj={
+        title:$$('[data-attr=title]').text(),
+        price:parseInt( $$('[data-attr=price]').text() ),
+        url:url,
+        image:$$('[data-attr=image]').text(),
+      }
+
+      if($$(this).find('i').text()=='favorite'){
+        $$(this).find('i').text('favorite_border');
+        removeFavorite(url);
+        $$('.page-previous a[href="'+url+'"]').hide();
+        app.toast.create({text: 'Usunięto z listy <a href="/favorites/">ulubione</a>.',position: 'bottom',closeTimeout: 3000,}).open()
+      }else{
+        $$(this).find('i').text('favorite');
+        addFavorite(favobj)
+        $$('.page-previous a[href="'+url+'"]').show();
+        app.toast.create({text: 'Dodano do listy <a href="/favorites/">ulubione</a>.',position: 'bottom',closeTimeout: 3000,}).open()
+      }
+
+    });
+
+
+    // Init/Create main view
+    var mainView = app.views.create('.view-main', {
+      url: '/home/',
+      pushState: true
+    });
+
+    if(!document.location.hash)app.router.navigate('/home/');
+    $$('.favorites-count').text( getFavorites().length );
+}
+
+function inapp(){
+  cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+}
+
+function getFavorites(){
+  favorites = JSON.parse(localStorage.getItem("favorites"));
+  if(favorites == null) return [];
+  return favorites;
+}
+
+function setFavorites(favorites){
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  $$('.favorites-count').text( getFavorites().length );
+}
+
+function isFavorited(url){
+  favorites = getFavorites();
+  if( favorites.length>0 ){
+    for(i=0;i<favorites.length;i++){
+      if(favorites[i].url == url) return true;
+    }
+  }
+  return false;
+}
+
+function addFavorite(favobj){
+  if(!isFavorited(favobj.url)){
+    favorites = getFavorites();
+    favorites.unshift(favobj);
+    setFavorites(favorites);
+  }
+}
+
+function removeFavorite(url){
+  favorites = getFavorites();
+  if( favorites.length>0 ){
+    for(i=0;i<favorites.length;i++){
+      if(favorites[i].url == url){
+        favorites.splice(i,1);
+        setFavorites(favorites);
+        return true;
+      } 
+    }
+  }
+  return false;
+}
---
+++ platforms/browser/www/js/routes.js
@@ -0,0 +1,323 @@
+routes = [
+  /*{
+    path: '/',
+    url: './index.html',
+  },*/
+
+  {
+    path: '/home/',
+    options: {
+      animate: false,
+    },    
+    async: function (routeTo, routeFrom, resolve, reject) {
+      var router = this;
+      var app = router.app;
+
+      if(app.data !== undefined && !app.data.list_home){
+        app.preloader.show();
+        app.request.getJSON('https://www.tanie-loty.com.pl/?option=com_okazje&view=home&format=raw&action=filters',null,function(result){
+
+          app.data.data_filters = result['filters'];
+
+          if( app.form.getFormData("filters_form") ){
+            mode = app.form.getFormData("filters_form").mode;
+          }
+          if(typeof mode == 'undefined')mode='simple';
+
+          resolve(
+            {
+              templateUrl: './pages/searchform.html',
+            },
+            {
+              context: { filters: result['filters'], mode: mode }
+            }            
+          );
+          //setTimeout("updateFilters(app.data.data_filters);",1000);
+          app.preloader.hide();
+
+        },function(){
+            //error 
+        });
+      }else{
+          resolve(
+            {
+              templateUrl: './pages/searchform.html',
+            },
+            {
+              context: {
+                list: app.data.list_home,
+              }
+            }            
+          );        
+      }
+
+    },
+  },
+
+  {
+    path: '/okazje/entries/:Cart/:Url',
+    //url: './pages/pakiet.html',
+
+    async: function (routeTo, routeFrom, resolve, reject) {
+      var router = this;
+      var app = router.app;
+
+      app.preloader.show();
+      app.request.getJSON('https://www.tanie-loty.com.pl/okazje/'+routeTo.params.Cart+'/'+routeTo.params.Url+'?app=true',null,function(result){
+
+
+        switch(result.type){
+          case 'tour':{tplname='tour.html';break;}
+          case 'flight':{tplname='flight.html';break;}
+          default:{tplname='package.html';}
+        }
+
+        resolve( { templateUrl: './pages/'+tplname }, { context: {
+          result: result,
+          img_base: 'https://img.tanie-loty.com.pl/media/slir/w'+(Math.min(960,Math.ceil($$('#app').width()/20)*40))+'-q80/',
+          shareable: navigator.share,
+          last_list: document.last_list?document.last_list:'/home/',
+          favorited: isFavorited(routeTo.path),
+        } });
+
+        console.log('https://img.tanie-loty.com.pl/media/slir/w'+(Math.min(960,Math.ceil($$('#app').width()/20)*40))+'-q80/'+result.image);
+        app.preloader.hide();
+
+      },function(){
+          //error 
+      });
+    }
+
+  }, 
+
+
+  {
+    path: '/okazje/entries/:Url',
+    //url: './pages/pakiet.html',
+
+    async: function (routeTo, routeFrom, resolve, reject) {
+      var router = this;
+      var app = router.app;
+
+      app.preloader.show();
+      app.request.getJSON('https://www.tanie-loty.com.pl/okazje/'+routeTo.params.Url+'?app=true',null,function(result){
+
+
+        switch(result.type){
+          case 'tour':{tplname='tour.html';break;}
+          case 'flight':{tplname='flight.html';break;}
+          default:{tplname='package.html';}
+        }
+
+        resolve( { templateUrl: './pages/'+tplname }, { context: {
+          result: result,
+          img_base: 'https://img.tanie-loty.com.pl/media/slir/w'+(Math.min(960,Math.ceil($$('#app').width()/20)*40))+'-q80/',
+          shareable: navigator.share,
+          last_list: document.last_list?document.last_list:'/home/'
+
+        } });
+        app.preloader.hide();
+
+      },function(){
+          //error 
+      });
+    }
+
+  }, 
+
+  {
+    path: '/okazje/lists/:Url',
+    //url: './pages/pakiet.html',
+
+    async: function (routeTo, routeFrom, resolve, reject) {
+      document.last_list='/okazje/lists/'+routeTo.params.Url;
+      var router = this;
+      var app = router.app;
+
+        app.preloader.show();
+
+        if(routeTo.params.Url.indexOf('&')>0){
+          url = 'https://www.tanie-loty.com.pl/okazje/?'+routeTo.params.Url;  
+        }else{
+          url = 'https://www.tanie-loty.com.pl/okazje/'+routeTo.params.Url+'?app=true';  
+        }
+        
+
+        app.request.getJSON(url,null,function(result){
+
+          for (var key in result.list) {
+            var obj=result.list[key];
+            switch(obj._type){
+              case 'pakiety':
+              case 'wycieczki':
+              case 'loty':
+              {
+                url=obj._url.replace('/okazje/','/okazje/entries/');
+                break;
+              }
+              default:{
+                url='404';
+              }
+            }
+
+            if(obj._type=='koszyki'){
+              card_price='od <b>'+obj.lowest_price+'</b> zł';
+            }else{
+              card_price='<b>'+obj.price+'</b> zł';
+            }
+
+            imgparams='w'+(Math.min(960,Math.ceil($$('#app').width()/20)*40))+'-q80';
+            obj.rendered_html='<a href="'+url+'" class="card"><div data-background="'+obj._image_url.replace('{params}',imgparams).replace('w480-h240-q95-c2x1',imgparams)+'" class="card-header card-header-image align-items-flex-end lazy lazy-fade-in"></div><div class="card-content card-content-padding"><p><b>'+obj.title+'</b></p></div><div class="card-footer"><span class="card-price">'+card_price+'</span><span class="col button button-fill">Sprawdź</span></div></a>';
+
+          }
+
+          resolve(
+            {
+              templateUrl: './pages/list.html',
+            },
+            {
+              context: { list: result.list, title: result.title, type: 'items' }
+            }            
+          );
+          app.preloader.hide();
+
+        },function(){
+            //error 
+        });
+      
+
+    }
+
+  },    
+
+  {
+    path: '/favorites/',
+    //url: './pages/pakiet.html',
+
+    async: function (routeTo, routeFrom, resolve, reject) {
+      document.last_list='/favorites/';
+      var router = this;
+      var app = router.app;
+        app.preloader.show();
+
+/*        if(routeTo.params.Url.indexOf('&')>0){
+          url = 'https://www.tanie-loty.com.pl/okazje/?'+routeTo.params.Url;  
+        }else{
+          url = 'https://www.tanie-loty.com.pl/okazje/'+routeTo.params.Url+'?app=true';  
+        }*/
+
+        favorites = getFavorites();
+        
+          for (var key in favorites) {
+            var obj=favorites[key];
+/*            switch(obj._type){
+              case 'pakiety':
+              case 'wycieczki':
+              case 'loty':
+              {
+                url=obj._url.replace('/okazje/','/okazje/entries/');
+                break;
+              }
+              default:{
+                url='404';
+              }
+            }*/
+
+/*            if(obj._type=='koszyki'){
+              card_price='od <b>'+obj.lowest_price+'</b> zł';
+            }else{*/
+              card_price='<b>'+obj.price+'</b> zł';
+/*            }*/
+
+            //imgparams='w'+(Math.min(960,Math.ceil($$('#app').width()/20)*40))+'-q80';
+            obj.rendered_html='<a href="'+obj.url+'" class="card"><div data-background="'+obj.image+'" class="card-header card-header-image align-items-flex-end lazy lazy-fade-in"></div><div class="card-content card-content-padding"><p><b>'+obj.title+'</b></p></div><div class="card-footer"><span class="card-price">'+card_price+'</span><span class="col button button-fill">Sprawdź</span></div></a>';
+
+          }
+
+          resolve(
+            {
+              templateUrl: './pages/list.html',
+            },
+            {
+              context: { list: favorites, title: 'Ulubione', type: 'favorites' }
+            }            
+          );
+          app.preloader.hide();
+
+    }
+
+  },  
+
+/*  {
+    path: '/about/',
+    url: './pages/temp-content.html',
+  },
+  {
+    path: '/form/',
+    url: './pages/form.html',
+  },
+  // Page Loaders & Router
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = routeTo.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/request-and-load.html',
+          },
+          {
+            context: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },*/
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
